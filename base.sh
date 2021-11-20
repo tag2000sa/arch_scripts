49e0df58c16c7fb1f7d1997dc6df8e961da2e250
tag2000sa <tag2000sa@users.noreply.github.com>
2021-11-20 12:38:37 +0300

    fix printing

--- base.sh
+++ base.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 printf "\e[1;36mWelcome to AutoInstaller Script.\n\e[0m"
-sleep(2)
+sleep 2
 
 # create swap-file
-printf "\e[1;32Create Swap-File.\n\e[0m"
-sleep(2)
+printf "\e[1;32mCreate Swap-File.\n\e[0m"
+sleep 2
 dd if=/dev/zero of=/swapfile bs=1M count=2048
 mkswap /swapfile
 chmod 600 /swapfile
@@ -13,7 +13,7 @@ echo -e "/swapfile\tnone\tswap\tdefaults\t0\t\t0" >> /etc/fstab
 
 # set time-zone
 printf "\e[1;32mSet TimeZone.\n\e[0m"
-sleep(2)
+sleep 2
 ln -sf /usr/share/zoneinfo/Asia/Riyadh /etc/localtime
 
 # set hardware clock
@@ -22,7 +22,7 @@ hwclock --systohc
 
 # set locale languages
 printf "\e[1;32mConfigure Locales.\n\e[0m"
-sleep(2)
+sleep 2
 sed -i 's/^\#ar\_SA\.UTF\-8/ar\_SA\.UTF\-8/; 62s/^\#en\_US\.UTF\-8/en\_US\.UTF\-8/;' /etc/locale.gen  
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
@@ -30,24 +30,24 @@ echo "KEYMAP=us" >> /etc/vconsole.conf
 
 # set PC name
 printf "\e[1;32mSet PC name.\n\e[0m"
-sleep(2)
+sleep 2
 echo "arch" >> /etc/hostname
 
 # set localhost
 printf "\e[1;32mConfigure localhost.\n\e[0m"
-sleep(2)
+sleep 2
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1	localhost" >> /etc/hosts
 echo "127.0.0.1 arch.localdomain arch" >> /etc/hosts
 
 # set root password to be 123
 printf "\e[1;32mSet root password.\n\e[0m"
-sleep(2)
+sleep 2
 echo root:123 | chpasswd
 
 # select all pakages to install 
 printf "\e[1;32mInstall all needed packages.\n\e[0m"
-sleep(2)
+sleep 2
 # enable parallel downloads
 sed -i 's/^\#ParallelDownloads\ \=\ 5/ParallelDownloads\ \=\ 10/' /etc/pacman.conf
 #pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-lts-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio xorg pavucontrol bash-completion openssh rsync openssh acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf openbsd-netcat vde2 dnsmasq bridge-utils ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
@@ -55,7 +55,7 @@ pacman -S --noconfirm grub efibootmgr networkmanager wpa_supplicant mtools dosfs
 
 # select video-graphic driver
 printf "\e[1;32mInstall VGA driver.\n\e[0m"
-sleep(2)
+sleep 2
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 # pacman -S --noconfirm xf86-video-intel
@@ -64,13 +64,13 @@ pacman -S --noconfirm xf86-video-vmware
 
 # install grub
 printf "\e[1;32mInstall GRUB.\n\e[0m"
-sleep(2)
+sleep 2
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # enable services
 printf "\e[1;32mEnable Services\n\e[0m"
-sleep(2)
+sleep 2
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable cups.service
@@ -85,7 +85,7 @@ systemctl enable acpid
 
 # create new user
 printf "\e[1;32mCreate new user (user)\n\e[0m"
-sleep(2)
+sleep 2
 user="user"
 useradd -m $user
 echo $user:123 | chpasswd
